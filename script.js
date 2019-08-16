b1923d1c96d7828c316f350458a7bc2c171371c6
Mustafa Haidari <mhaidarpoor@gmail.com>
2019-08-16 13:55:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-var countDownDate = new Date("August 29, 2019 23:05:00").getTime();
+var countDownDate = new Date("August 29, 2019 11:05:00").getTime();
+var countDownDateT = new Date("August 29, 2019 11:05:00");
+var countDownDateTag = document.getElementById("countDownDate").innerHTML = countDownDateT.toDateString() + " at " + countDownDateT.getHours() + ":" + String(countDownDateT.getMinutes()).padStart(2, "0") + "am";
+console.log(String(countDownDateT.getMinutes()).padStart(2, "0"))
 
-	var x = setInterval(function() {
-		var now = new Date().getTime();
-		var distance = countDownDate - now;
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var x = setInterval(function () {
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-			minutes + "m " + seconds + "s ";
+	document.getElementById("demo").innerHTML = days + "<span><sup>D</sup></span> " + hours + "<span><sup>H</sup></span> " +
+		minutes + "<span><sup>M</sup></span> " + seconds + "<span><sup>S</sup></span>";
 
-		if (distance < 0) {
-			clearInterval(x);
-			document.getElementById("demo").innerHTML = "Malcolm is on his way to the United States of America!";
-		}
-	}, 1000);
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("demo").innerHTML = "Malcolm is on his way to the United States of America!";
+	}
+}, 1000);
